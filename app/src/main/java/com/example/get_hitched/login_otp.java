9fcbae0e63b1fd4e6b3868be1bfbbad9530aaea5
audist <arunjunanvenkatesan@gmail.com>
2025-02-12 19:45:29 +0545

    update login_otp

--- app/src/main/java/com/example/get_hitched/login_otp.java
+++ app/src/main/java/com/example/get_hitched/login_otp.java
@@ -4,25 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 import com.chaos.view.PinView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
 import java.util.concurrent.TimeUnit;
 
 public class login_otp extends AppCompatActivity {
 
     PinView pinFromUser;
-    String codeBySystem;
-
+    String otpid;
     private FirebaseAuth mAuth;
 
-
+    String phonenumber;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,86 +29,65 @@ public class login_otp extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         pinFromUser = findViewById(R.id.pin_view);
-        String _phoneNo = getIntent().getStringExtra("mobile");
-        sendVerificationCodeToUser(_phoneNo);
+
+
+        phonenumber = getIntent().getStringExtra("mobile").toString();
+        mAuth = FirebaseAuth.getInstance();
+        // txt_alert.setVisibility(View.GONE);
+
+        //final String verificationId = getIntent().getStringExtra("verificationId");
+        String number = getIntent().getStringExtra("mobile");
+
+        initiate();
+
 
     }
 
-    private void sendVerificationCodeToUser(String phoneNo) {
-        if (phoneNo == null || phoneNo.trim().isEmpty()) {
-            Toast.makeText(this, "Invalid phone number!", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        PhoneAuthOptions options = PhoneAuthOptions.newBuilder(mAuth)
-                .setPhoneNumber(phoneNo)       // Phone number to verify
-                .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
-                .setActivity(this)                 // Activity (for callback binding)
-                .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
-                .build();
-        PhoneAuthProvider.verifyPhoneNumber(options);
-
-    }
-
-    private final PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks =
-            new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
-                @Override
-                public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                    super.onCodeSent(s, forceResendingToken);
-                    codeBySystem = s;
-                }
-
-                @Override
-                public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                    String code = phoneAuthCredential.getSmsCode();
-                    if (code != null) {
-                        pinFromUser.setText(code);
-                        verifyCode(code);
+    private void initiate()
+    {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phonenumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks()
+                {
+                    @Override
+                    public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken)
+                    {
+                        otpid=s;
                     }
 
-                }
+                    @Override
+                    public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential)
+                    {
+                        signInWithPhoneAuthCredential(phoneAuthCredential);
+                    }
 
-                @Override
-                public void onVerificationFailed(@NonNull FirebaseException e) {
-                    Toast.makeText(login_otp.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-
-                }
-            };
-
-    private void verifyCode(String code) {
-        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeBySystem, code);
-        signInWithPhoneAuthCredential(credential);
+                    @Override
+                    public void onVerificationFailed(FirebaseException e) {
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });        // OnVerificationStateChangedCallbacks
 
     }
+
 
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            startActivity(new Intent(login_otp.this,HomeActivity.class));
+                            finish();
 
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        Toast.makeText(login_otp.this, "Verification is completed ", Toast.LENGTH_SHORT).show();
-                    }
-                    else {
-                        if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
-                            Toast.makeText(login_otp.this, "Verification Not Completed! Try again.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Sign in Code Error",Toast.LENGTH_LONG).show();
                         }
                     }
                 });
-    }
-
-
-    public void callNextScreenFromOTP(View view) {
-        String code = pinFromUser.getText().toString();
-        if (!code.isEmpty()) {
-            verifyCode(code);
-        }
-    }
-
-    public void goTOHomeFromOTP (View view){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-
     }
 
 }
