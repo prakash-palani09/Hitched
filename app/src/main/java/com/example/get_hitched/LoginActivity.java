8f0c107b5c381701f7a9da67f61f1042344134d2
parakashpalani09 <prakashpalani009@gmail.com>
2025-02-18 12:12:14 +0545

    login finished

--- app/src/main/java/com/example/get_hitched/LoginActivity.java
+++ app/src/main/java/com/example/get_hitched/LoginActivity.java
@@ -1,42 +1,101 @@
 package com.example.get_hitched;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.hbb20.CountryCodePicker;
 
 public class LoginActivity extends AppCompatActivity {
-    CountryCodePicker countryCodePicker;
-    EditText inputMobile;
-    Button getOTP;
-    ProgressBar progressBar;
+    TextInputEditText etLoginEmail;
+    TextInputEditText etLoginPassword;
+    TextView tvRegisterHere;
+
+    Button btnLogin;
+    TextView tvForgotPassword;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        countryCodePicker = findViewById(R.id.login_countrycode);
-        inputMobile = findViewById(R.id.login_mobile_number);
-        getOTP = findViewById(R.id.send_otp_btn);
-        progressBar = findViewById(R.id.login_progress_bar);
 
-        progressBar.setVisibility(View.GONE);
+        etLoginEmail = findViewById(R.id.etLoginEmail);
+        etLoginPassword = findViewById(R.id.etLoginPass);
+        tvRegisterHere = findViewById(R.id.tvRegisterHere);
+        btnLogin = findViewById(R.id.btnLogin);
+        tvForgotPassword = findViewById(R.id.tvForgotPassword);
 
-        countryCodePicker.registerCarrierNumberEditText(inputMobile);
-        getOTP.setOnClickListener((v)->{
-            if(!countryCodePicker.isValidFullNumber()){
-                inputMobile.setError("Phone number not valid");
-                return;
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            // If user is already logged in, navigate to the main screen
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();  // Close the LoginActivity to prevent going back
+        }
+        btnLogin.setOnClickListener(view -> {
+            loginUser();
+        });
+
+        // Forgot password link click listener
+        tvForgotPassword.setOnClickListener(view -> {
+            String email = etLoginEmail.getText().toString();
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(LoginActivity.this, "Please enter your email address", Toast.LENGTH_SHORT).show();
+            } else {
+                sendPasswordResetEmail(email);
             }
-            Intent intent=new Intent(LoginActivity.this,login_otp.class);
-            intent.putExtra("mobile",countryCodePicker.getFullNumberWithPlus().replace(" ",""));
-            startActivity(intent);
         });
     }
 
+    private void loginUser(){
+        String email = etLoginEmail.getText().toString();
+        String password = etLoginPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            etLoginEmail.setError("Email cannot be empty");
+            etLoginEmail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            etLoginPassword.setError("Password cannot be empty");
+            etLoginPassword.requestFocus();
+        }else{
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendPasswordResetEmail(String email) {
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Password reset email sent", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
